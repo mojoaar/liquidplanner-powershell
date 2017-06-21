d86a34475aad845a942bf898dd7c5d9135a4fb71
Morten G. Johansen <mgj@syspeople.dk>
2017-06-21 22:05:34 +0200

    Filter parameter added

--- PSLiquidPlanner-Client.psm1
+++ PSLiquidPlanner-Client.psm1
@@ -5,17 +5,21 @@
     You must have invoked Set-LiquidPlannerAuth or Set-LiquidPlannerAuthToken prior to executing this cmdlet
 .PARAMETER Name
     Parameter to specify the name to search for. Operator is a contains condition. Optional Parameter.
+.PARAMETER Filter
+    Parameter to specify filter to use in the query. Optional Parameter.
 .EXAMPLE
     Return clients that contains the name Client ABC
         Get-LiquidPlannerClient -Name 'Client ABC'
 .EXAMPLE
-    Return all clients
-        Get-LiquidPlannerClient
+    Return clients that contains the name Microsoft
+        Get-LiquidPlannerClient -Filter '?filter[]=name%20contains%20Microsoft'
 #>
 function Get-LiquidPlannerClient {
     Param (
         [Parameter(Mandatory=$false)]
-        [string] $Name
+        [string] $Name,
+        [Parameter(Mandatory=$false)]
+        [string] $Filter
     )
     if (-not $Global:LiquidPlannerWorkspace) {
         'You need to set the Workspace Id with Set-LiquidPlannerWorkspace'
@@ -24,6 +28,8 @@ function Get-LiquidPlannerClient {
     $ClientURL = $Global:LiquidPlannerRESTURL + '/workspaces/' + $Global:LiquidPlannerWorkspace + '/clients/'
     if ($Name) {
         $ClientURL = $ClientURL + '?filter[]=name%20contains%20' + $Name
+    } elseif ($Filter) {
+        $ClientURL = $ClientURL + $Filter
     }
     $Header = @{
         Authorization = "Bearer $Global:LiquidPlannerToken"
