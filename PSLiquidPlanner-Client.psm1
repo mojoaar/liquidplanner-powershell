b11c09c4404fecb3622b8495a1461bcd47180c90
Morten G. Johansen <mgj@syspeople.dk>
2017-06-21 21:17:51 +0200

    New function added

---
+++ PSLiquidPlanner-Client.psm1
@@ -0,0 +1,32 @@
+<#
+.SYNOPSIS
+   Returns clients from the connected Liquid Planner URL (optionally based on a name)
+.NOTES
+   You must have invoked Set-LiquidPlannerAuth or Set-LiquidPlannerAuthToken prior to executing this cmdlet
+.EXAMPLE
+    Return clients that contains the name Client ABC
+        Get-LiquidPlannerClient -Name 'Client ABC'
+.EXAMPLE
+    Return all clients
+        Get-LiquidPlannerClient
+#>
+function Get-LiquidPlannerClient {
+    Param (
+        [Parameter(Mandatory=$false)]
+        [string] $Name
+    )
+    if (-not $Global:LiquidPlannerWorkspace) {
+        'You need to set the Workspace Id with Set-LiquidPlannerWorkspace'
+        break
+    }
+    $TaskURL = $Global:LiquidPlannerRESTURL + '/workspaces/' + $Global:LiquidPlannerWorkspace + '/clients/'
+    if ($Name) {
+        $TaskURL = $TaskURL + '?filter[]=name%20contains%20' + $Name
+    }
+    $Header = @{
+        Authorization = "Bearer $Global:LiquidPlannerToken"
+        Accept = "*/*"
+    }
+    $Result = Invoke-RestMethod -Method Get -Uri $TaskURL -ContentType "application/json" -Headers $Header
+    return $Result
+}
