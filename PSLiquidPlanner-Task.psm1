d1edc4208847139d7868bfa6af2ceea1e7ea8485
Morten G. Johansen <mgj@syspeople.dk>
2017-06-21 19:35:36 +0200

    Get-LiquidPlannerTask updated

--- PSLiquidPlanner-Task.psm1
+++ PSLiquidPlanner-Task.psm1
@@ -1,18 +1,25 @@
+<#
+.Synopsis
+   Returns tasks from the connected Liquid Planner URL (optionally based on a filter)
+.NOTES
+   You must have invoked Set-LiquidPlannerAuth or Set-LiquidPlannerAuthToken prior to executing this cmdlet
+.EXAMPLE
+    Return the incident whose number is exactly INC0010683
+        Get-LiquidPlannerTask -Filter '?filter[]=id=39235958'
+.EXAMPLE
+    Return all tasks that are not marked as done
+        Get-LiquidPlannerTask -Filter '?filter[]=is_done%20is%20false'
+#>
+
 function Get-LiquidPlannerTask {
     Param (
-        [Parameter(Mandatory=$false)]
-        [int] $Id,
         [Parameter(Mandatory=$false)]
         [string] $Filter
     )
     $TaskURL = $Global:LiquidPlannerRESTURL + '/workspaces/' + $Global:LiquidPlannerWorkspace + '/tasks/'
-    if ($Id) {
-        $TaskURL = $TaskURL + $Id
-    } elseif ($Filter) {
+    if ($Filter) {
         # Example use of filter: '?filter[]=is_done%20is%20false'
         $TaskURL = $TaskURL + $Filter
-    } elseif ($Id -and $Filter) {
-        $TaskURL = $TaskURL + $Id + $Filter
     }
     $Header = @{
         Authorization = "Bearer $Global:LiquidPlannerToken"
