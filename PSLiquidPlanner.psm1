5a93a30dc6d87049f9a6d980537df6b505fb06e8
Morten G. Johansen <mgj@syspeople.dk>
2017-06-22 07:24:58 +0200

    Help updated

--- PSLiquidPlanner.psm1
+++ PSLiquidPlanner.psm1
@@ -1,4 +1,11 @@
-function Test-LiquidPlannerAuthIsSet{
+<#
+.SYNOPSIS
+    Test if auth is set in the current session
+.EXAMPLE
+    Will check if auth is set either from credentials or a token
+        Test-LiquidPlannerAuthIsSet
+#>
+function Test-LiquidPlannerAuthIsSet {
     if($Global:LiquidPlannerCredentials -or $Global:LiquidPlannerToken){
         return $true;
     }else{
@@ -6,7 +13,16 @@ function Test-LiquidPlannerAuthIsSet{
     }
 }
 
-function Set-LiquidPlannerAuth{
+<#
+.SYNOPSIS
+    Set's the auth to your Liquid Planner URL using credentials
+.PARAMETER Creadentials
+    Parameter to specify the credentials to use. Mandatory Parameter.
+.EXAMPLE
+    Set the credentials to user@company.com
+        Set-LiquidPlannerAuth -Credentials 'user@company.com'
+#>
+function Set-LiquidPlannerAuth {
     param(
         [parameter(mandatory=$true)]
         [System.Management.Automation.PSCredential]$Credentials
@@ -16,7 +32,18 @@ function Set-LiquidPlannerAuth{
     return $true;
 }
 
-function Set-LiquidPlannerAuthToken{
+<#
+.SYNOPSIS
+    Set's the auth to your Liquid Planner URL using a token
+.NOTES
+    Tokens can be created under Settings -> My API Tokens in Liquid Planner
+.PARAMETER Token
+    Parameter to specify filter to use in the query. Optional Parameter.
+.EXAMPLE
+    Set's the auth token to 12a3bc4d-5678-9e0f-8c92-8affa74dd371
+        Set-LiquidPlannerAuthToken -Token '12a3bc4d-5678-9e0f-8c92-8affa74dd371'
+#>
+function Set-LiquidPlannerAuthToken {
     param(
         [parameter(mandatory=$true)]
         [string]$Token
@@ -26,7 +53,14 @@ function Set-LiquidPlannerAuthToken{
     return $true;
 }
 
-function Remove-LiquidPlannerAuth{
+<#
+.SYNOPSIS
+    Removes all Liquid Planner related variables
+.EXAMPLE
+    Will clean your environment and remove all Liquid Planner Global variables
+        Remove-LiquidPlannerAuth
+#>
+function Remove-LiquidPlannerAuth {
 
     Remove-Variable -Name LiquidPlannerRESTURL -Scope Global
     Remove-Variable -Name LiquidPlannerCredentials -Scope Global
