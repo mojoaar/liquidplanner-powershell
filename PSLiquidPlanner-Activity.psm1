268e155523d926a11a624c874eba0e5ce5e43fce
Morten G. Johansen <mgj@syspeople.dk>
2017-06-22 21:25:22 +0200

    PSLiquidPlanner-Activity.psm1 added

---
+++ PSLiquidPlanner-Activity.psm1
@@ -0,0 +1,22 @@
+<#
+.SYNOPSIS
+    Get activities from the connected Liquid Planner URL
+.NOTES
+    You must have invoked Set-LiquidPlannerAuth or Set-LiquidPlannerAuthToken prior to executing this cmdlet
+.EXAMPLE
+    Get-Get-LiquidPlannerActivity
+    Will get all activities of the connected Liquid Planner URL
+#>
+function Get-LiquidPlannerActivity {
+    $ActivityURL = $Global:LiquidPlannerRESTURL + '/workspaces/' + $Global:LiquidPlannerWorkspace + '/activities/'
+    if ($Global:LiquidPlannerToken) {
+        $Header = @{
+            Authorization = "Bearer $Global:LiquidPlannerToken"
+            Accept = "*/*"
+        }
+        $Result = Invoke-RestMethod -Method Get -Uri $ActivityURL -ContentType "application/json" -Headers $Header
+    } else {
+        $Result = Invoke-RestMethod -Method Get -Uri $ActivityURL -ContentType "application/json" -Credential $Global:LiquidPlannerCredentials
+    }
+    return $Result
+}
