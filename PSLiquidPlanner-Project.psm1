b70dc0749b5733a00d88d975f937623fb1d50c65
Morten G. Johansen <mgj@syspeople.dk>
2017-07-12 09:52:34 +0200

    New-LiquidPlannerProject function added + version bump

--- PSLiquidPlanner-Project.psm1
+++ PSLiquidPlanner-Project.psm1
@@ -50,3 +50,98 @@ function Get-LiquidPlannerProject {
     }
     return $Result
 }
+
+<#
+.SYNOPSIS
+    Creates new project in the connected Liquid Planner URL
+.NOTES
+    You must have invoked Set-LiquidPlannerAuth or Set-LiquidPlannerAuthToken prior to executing this cmdlet
+.PARAMETER Name
+    Parameter to set the name of the new Liquid Planner project. Mandatory Parameter.
+.PARAMETER Description
+    Parameter to set the description of the new Liquid Planner project. Mandatory Parameter.
+.PARAMETER PersonId
+    Parameter to set assignment of the new Liquid Planner project. Optional Parameter.
+.PARAMETER Reference
+    Parameter to set the external_reference field of the new Liquid Planner project. Optional Parameter.
+.PARAMETER ClientId
+    Parameter to set the client_id field of the new Liquid Planner project. Optional Parameter.
+.PARAMETER ParentId
+    Parameter to set the parent_id field of the new Liquid Planner project. Optional Parameter.
+.EXAMPLE
+    New-LiquidPlannerProject -Name 'Testing' -Description 'Just a test'
+    Creates a new project with the name Testing and a description saying Just a test, will set the assignment to unassigned
+.EXAMPLE
+    New-LiquidPlannerProject -Name 'Testing' -Description 'Just a test' -PersonId '0'
+    Creates a new project with the name Testing and a description saying Just a test, will assign to Person Id 0 (unassigned)
+.EXAMPLE
+    New-LiquidPlannerProject -Name 'Testing' -Description 'Just a test' -Reference 'Ref Test'
+    Creates a new project with the name Testing and a description saying Just a test, will set the reference to Ref Test
+.EXAMPLE
+    New-LiquidPlannerProject -Name 'Testing' -Description 'Just a test' -Reference 'Ref Test' -PersonId '123' -ClienId '456' -ParentId '789'
+    Creates a new project with the name Testing, a description saying Just a test, set's the reference to Ref Test, assign to person 123, link to customer 456 and put's it under parent 789
+#>
+function New-LiquidPlannerProject {
+    Param (
+        [Parameter(Mandatory=$true)]
+        [string] $Name,
+        [Parameter(Mandatory=$true)]
+        [string] $Description,
+        [Parameter(Mandatory=$false)]
+        [string] $PersonId,
+        [Parameter(Mandatory=$false)]
+        [string] $Reference,
+        [Parameter(Mandatory=$false)]
+        [string] $ClientId,
+        [Parameter(Mandatory=$false)]
+        [string] $ParentId
+    )
+    if ((Test-LiquidPlannerAuthIsSet) -eq $false) {
+        'You need to set the Authorization with Set-LiquidPlannerAuthToken or Set-LiquidPlannerAuth'
+        break
+    }
+    if (-not $Global:LiquidPlannerWorkspace) {
+        'You need to set the Workspace Id with Set-LiquidPlannerWorkspace'
+        break
+    }
+    $ProjectURL = $Global:LiquidPlannerRESTURL + '/workspaces/' + $Global:LiquidPlannerWorkspace + '/projects/'
+    if ($PersonId) {
+    $Body = @{
+        project = @{
+            name = "$Name"
+            description = "$Description"
+            external_reference = "$Reference"
+            client_id = "$ClientId"
+            parent_id = "$ParentId"
+                assignments = @(@{
+                    person_id = "$PersonId"
+                })
+        }
+    }
+    } else {
+    $Body = @{
+        project = @{
+            name = "$Name"
+            description = "$Description"
+            external_reference = "$Reference"
+            client_id = "$ClientId"
+            parent_id = "$ParentId"
+                assignments = @(@{
+                    person_id = "0"
+                })
+        }
+    }
+    }
+    $Body = ConvertTo-Json -InputObject $Body -Depth 10
+    $Body = [System.Text.Encoding]::UTf8.GetBytes($Body)
+    if ($Global:LiquidPlannerToken) {
+        $Header = @{
+            Authorization = "Bearer $Global:LiquidPlannerToken"
+            Accept = "*/*"
+        }
+        $Result = Invoke-RestMethod -Method Post -Uri $ProjectURL -ContentType "application/json" -Headers $Header -Body $Body
+    } else {
+        $Result = Invoke-RestMethod -Method Post -Uri $ProjectURL -ContentType "application/json" -Credential $Global:LiquidPlannerCredentials -Body $Body
+    }
+    return $Result
+}
